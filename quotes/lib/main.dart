f0475d4c4fccab8fb7eb98839444fc1bfb0cd982
cristinaponay <cristinaponay@gmail.com>
2020-02-13 18:40:34 -0400

    used card widget to display data from objects

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quotes_card.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -10,8 +12,33 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Stephanie Piro', text: 'Behind every successful woman is a substantial amount of coffee.'),
+    Quote(author: 'Josh Billings', text: 'Never work before breakfast; if you have to work before breakfast, eat your breakfast first.'),
+    Quote(author: 'Zooey Deschanel', text: 'In an ideal world no one would talk before 10am. People would just hug, because waking up is really hard.')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
+      ),
+    );
   }
 }
+
