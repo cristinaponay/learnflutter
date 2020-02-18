2a8401aaae3d8918da1eb39c982a90cafe2b745c
cristinaponay <cristinaponay@gmail.com>
2020-02-18 19:15:22 -0400

    signing in as anonymous user

---
+++ brew_crew/lib/services/auth.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:brew_crew/models/user.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // create user object based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+        // .map((FirebaseUser user) => _userFromFirebaseUser(user));  // the same as below
+        .map(_userFromFirebaseUser); // alternative way to write it
+  }
+
+  // sign in anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email and password
+
+  // register with email and password
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
