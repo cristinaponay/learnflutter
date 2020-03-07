9a4374933773f78823ffb8ed7562201e2a90cb76
cristinaponay <cristinaponay@gmail.com>
2020-03-07 16:00:36 -0400

    removed some projects from repo

--- dreamcharts/lib/models/dream.dart
+++
@@ -1,8 +0,0 @@
-class Dream {
-  final String date;
-  final String title;
-  final String report;
-  final List<String> categories;
-
-  Dream({this.date, this.title, this.report, this.categories});
-}
